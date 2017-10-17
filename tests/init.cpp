c21982973807be80cc2f8200d700c8519dd68b5f
SVolkoff <sabina.volkoff@gmail.com>
2017-10-17 19:33:55 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -47,13 +47,16 @@ SCENARIO("pop")
 	st.push(1);
 	st.push(2);
 	st.push(3);
-	int val = st.pop();
+	int val = st.top();
+	st.pop();
 	REQUIRE(st.count() == 2);
 	REQUIRE(val == 3);
-	val = st.pop();
+	val = st.top();
+	st.pop();
 	REQUIRE(st.count() == 1);
 	REQUIRE(val == 2);
-	val = st.pop();
+	val = st.top();
+	st.pop();
 	REQUIRE(st.count() == 0);
 	REQUIRE(val == 1);
 }
